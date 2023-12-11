bbdbe7703615c18350618f55d22c2fcc7f230d33
WebMorphers <145851559+WebMorphers@users.noreply.github.com>
2023-12-11 02:28:21 -0800

    Update main.tsx

--- portfolio2.0/src/main.tsx
+++ portfolio2.0/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 <Analytics />
 
@@ -12,6 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   
   <React.StrictMode> 
     <SpeedInsights/>
+    <Analytics />
+
     <App /> 
   </React.StrictMode>,
 )
