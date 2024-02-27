9bfc02ddfbf4e2aef6cb4ed9da8cbecdfa43d76e
WebMorphers <145851559+WebMorphers@users.noreply.github.com>
2024-02-27 14:22:30 +0100

    k

--- portfolio2.0/src/components/team/team.tsx
+++ portfolio2.0/src/components/team/team.tsx
@@ -1,4 +1,4 @@
-import moncef from "../../assets/moncef.jpg";
+import moncef from "../../assets/22.png";
 import othman from "../../assets/othman.jpg";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
@@ -24,7 +24,7 @@ const team = () => {
         transition={{ duration: 0.5 }}
       >
         <div className="  py-24 sm:py-32 team">
-        <h1 className="text-4xl max-md:text-3xl text-center">            Meet our leadership
+        <h1 className="text-4xl max-md:text-3xl text-center">Meet our leadership
           </h1>
           <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-2 py-10">
             <div className="flex items-center col-span-2">
@@ -33,7 +33,7 @@ const team = () => {
               className="grid w-full lg:px-9 gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2 text"
             >
               
-                <li>
+                <li><a href="https://www.linkedin.com/in/monsif-souri-1b9a2a206/" target="blank-_self">
                   <div className="flex items-center justify-center gap-x-6">
                     <img
                       className="h-20 w-20 rounded-full object-cover"
@@ -49,6 +49,7 @@ const team = () => {
                       </p>
                     </div>
                   </div>
+                  </a>
                 </li>
               
                 <li>
